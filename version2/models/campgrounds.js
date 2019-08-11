14df04ff74c2761e831af86ff982bef0179e5973
Naman Dangi <danginaman55@gmail.com>
2019-08-11 05:09:49 +0000

    Associate comment and campground data

--- version2/models/campgrounds.js
+++ version2/models/campgrounds.js
@@ -3,7 +3,13 @@ var mongoose = require("mongoose");
 var campgroundSchema=mongoose.Schema({
 	name:String,
 	image:String,
-	description:String
+	description:String,
+	comments :[
+		{
+			type : mongoose.Schema.type.ObjectId,
+			ref : "Comments"
+		}
+	]
 });
 
 module.exports = mongoose.model("Campground",campgroundSchema);	
