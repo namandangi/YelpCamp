9ac37a24ad1da9a7d0e732dc317b0411c06a3321
Naman Dangi <namandangi@users.noreply.github.com>
2019-08-22 23:34:53 +0530

    Create seperate route-APIs and export to app.js

---
+++ version5/routes/comments.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var router  = express.Router({mergeParams:true});   //  *important --since we have reduced route names we will have problem with passing id 
+var Campground  = require("../models/campgrounds");
+
+router.get("/new", isLoggedIn, function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("./comments/comment", {campground: campground});
+        }
+    })
+});
+
+router.post("",isLoggedIn,function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports  = router;
