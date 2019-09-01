6ae8f09693e27dc6bd3b07fe0615a4f23371ec69
Naman Dangi <namandangi@users.noreply.github.com>
2019-09-01 20:39:49 +0530

    Add additional dummy data for database

--- version5/seeds.js
+++ version5/seeds.js
@@ -7,7 +7,10 @@ var data =[
 	{
 	   name: "Cloud's Rest", 
        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+	   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+	   author:{
+		   username:"namandangi"
+	   }
 	},
 	{
         name: "Canyon Floor", 
@@ -21,8 +24,7 @@ var data =[
 ]
 function seedDB()
 {	//Remove all campgrounds
-	Campground.remove({},function(err)
-    {
+	Campground.remove({},function(err){
 		if(err)
 			{
 				console.log(err);
